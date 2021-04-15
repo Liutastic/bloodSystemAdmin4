ec30b5573bd13ef25f53aafd05ce3b92d77e3599
wenruiweiDD <597367976@qq.com>
2021-04-15 18:17:38 +0800

    fix:菜单修改

--- src/menu/index.js
+++ src/menu/index.js
@@ -1,11 +1,13 @@
 import { uniqueId } from 'lodash'
+import header from '@/menu/header'
+import aside from '@/menu/aside'
 
 /**
  * @description 给菜单数据补充上 path 字段
  * @description https://github.com/d2-projects/d2-admin/issues/209
  * @param {Array} menu 原始的菜单数据
  */
-function supplementPath (menu) {
+export function supplementPath (menu) {
   return menu.map(e => ({
     ...e,
     path: e.path || uniqueId('d2-menu-empty-'),
@@ -15,41 +17,6 @@ function supplementPath (menu) {
   }))
 }
 
-export const menuHeader = supplementPath([
-  { path: '/index', title: '首页', icon: 'home' },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page3', title: '页面 3' }
-    ]
-  },
-  {
-    title: '页面2',
-    icon: 'folder-o',
-    children: [
-      { path: '/page2', title: '页面 2' }
-    ]
-  }
+export const menuHeader = supplementPath(header)
 
-])
-
-export const menuAside = supplementPath([
-  { path: '/index', title: '首页', icon: 'home' },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page3', title: '页面 3' }
-    ]
-  },
-  {
-    title: '页面2',
-    icon: 'folder-o',
-    children: [
-      { path: '/page2', title: '页面 2' }
-    ]
-  }
-])
+export const menuAside = supplementPath(aside)
---
+++ src/views/d1/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <d2-container>
+    <template slot="header">d1</template>
+    Hello World
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'd2111111'
+}
+</script>
